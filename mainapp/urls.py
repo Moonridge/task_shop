e6f44f0236714d7a3642b6b335d4504062449f71
Moonridge <61801710+Moonridge@users.noreply.github.com>
2021-07-23 18:54:55 +0300

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', BaseView.as_view(), name = 'index'),
     path('items/<str:slug>/', ItemDetailView.as_view(), name = 'detail'),
     path('add_sale/<str:slug>/', AddSaleView.as_view(), name = 'add_sale'),
+    path('sales/', SaleInfo.as_view(), name = 'sales'),
 ]
