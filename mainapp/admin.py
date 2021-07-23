4ea5d80770aa903d17be513d9e35eef6a13f48f9
Moonridge <61801710+Moonridge@users.noreply.github.com>
2021-07-23 22:08:21 +0300

    Add files via upload

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Item, Employee, Sale
+from .models import Item, Employee, Sale, Price
 
 admin.site.register(Item)
 admin.site.register(Employee)
 admin.site.register(Sale)
+admin.site.register(Price)
