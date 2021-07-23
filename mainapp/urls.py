078331053429426a6e81754cbb72ae1708b4fdb1
Moonridge <61801710+Moonridge@users.noreply.github.com>
2021-07-23 18:55:13 +0300

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BaseView, ItemDetailView, AddSaleView
+from .views import BaseView, ItemDetailView, AddSaleView, SaleInfo
 
 app_name = 'mainapp'
 urlpatterns = [
