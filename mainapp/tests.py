ea6c65ae6f3ce3a1c6599b72b3297383973db0c6
Moonridge <61801710+Moonridge@users.noreply.github.com>
2021-07-29 03:56:09 +0300

    Add files via upload

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from .models import Item, Employee, Sale, Price
 
-# Create your tests here.
+User = get_user_model()
+
+class ShopTestCases(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(username = "testuser", password = "password")
+        self.item = Item.objects.create(
+            item_name = "test name",
+            item_description = "test description",
+            item_price = "10",
+            slug = "testslug",
+        )
+        self.employee = Employee.objects.create(employee_name = "test name")
+        self.sale = Sale.objects.create(
+            sale_item = self.item,
+            sale_employee = self.employee,
+        )
+
+    def test_response_from_start_page(self):
+        client = Client()
+        response = client.get('')
+        self.assertEqual(response.status_code, 200)
