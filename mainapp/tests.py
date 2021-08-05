2399302741f9d5d862476308f3630cb9100173dc
Moonridge <61801710+Moonridge@users.noreply.github.com>
2021-08-06 01:56:32 +0300

    Add files via upload

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,33 +1,32 @@
-from django.test import TestCase, Client
-from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.contrib.auth.models import User
 from .models import Item, Employee, Sale, Price
 
-User = get_user_model()
-client = Client()
+
 
 class ShopTestCases(TestCase):
 
     def setUp(self) -> None:
-        self.user = User.objects.create(username = "testuser", password = "password")
+        self.user = User.objects.create_user(username = "testuser", password = "password")
         self.item = Item.objects.create(
             item_name = "test name",
             item_description = "test description",
-            item_price = "10",
+            item_price = 10,
             slug = "testslug",
         )
         self.employee = Employee.objects.create(employee_name = "test name")
 
     def test_response_from_start_page(self):
-        response = client.get('')
+        response = self.client.get('')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.item.item_name)
-        
+
     def test_detail_response(self):
-        response = client.get('/items/testslug/')
+        response = self.client.get('/items/testslug/')
         self.assertEqual(response.status_code, 200)
 
     def test_sale_form_post(self):
-        response = client.post('/add_sale/testslug/', {
+        response = self.client.post('/add_sale/testslug/', {
             'slug': self.item.slug,
             'employee': self.employee.employee_name,
             'qty' : '4',
@@ -39,18 +38,20 @@ class ShopTestCases(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_sale_response(self):
-        client.post('/login/', {
+        response = self.client.get('/sales/')
+        self.assertEqual(response.status_code, 302)
+        self.client.post('/login/', {
             'name': self.user.username,
             'password': self.user.password,
         }, follow = True)
-        response = client.get('/sales/', follow = True)
+        response = self.client.get('/sales/', follow = True)
         self.assertEqual(response.status_code, 200)
 
     def test_price_save(self):
         start_price = Price.objects.get(id = 1)
-        self.assertEqual(self.item.item_price, str(start_price.new_price))
+        self.assertEqual(self.item.item_price, start_price.new_price)
         updated_item_price = self.item
-        updated_item_price.item_price = '20'
+        updated_item_price.item_price = 20
         updated_item_price.save()
-        new_price = Price.objects.get(new_price = '20')
-        self.assertEqual(updated_item_price.item_price, str(new_price.new_price))
+        new_price = Price.objects.get(new_price = 20)
+        self.assertEqual(updated_item_price.item_price, new_price.new_price)
